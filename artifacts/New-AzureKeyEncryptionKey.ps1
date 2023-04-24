a393e753a9d1b80eb05e43061843dc287df6e83f
Jason Masten <jamasten@microsoft.com>
2023-04-24 17:23:19 -0400

    Updated kek to support modern operating systems

--- artifacts/New-AzureKeyEncryptionKey.ps1
+++ artifacts/New-AzureKeyEncryptionKey.ps1
@@ -5,7 +5,7 @@ param(
 
 if(!(Get-AzKeyVaultKey -Name 'DiskEncryption' -VaultName $KeyVault))
 {
-    Add-AzKeyVaultKey -Name 'DiskEncryption' -VaultName $KeyVault -Destination Software
+    Add-AzKeyVaultKey -Name 'DiskEncryption' -VaultName $KeyVault -Destination 'Software' -KeyType 'RSA' -Size 4096
 }
 $KeyEncryptionKeyURL = (Get-AzKeyVaultKey -VaultName $KeyVault -Name 'DiskEncryption' -IncludeVersions | Where-Object {$_.Enabled -eq $true}).Id
 Write-Output $KeyEncryptionKeyURL
